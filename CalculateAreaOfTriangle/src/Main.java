1f5b7e0671857b027a8d5b687089b14647239f8d
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2024-01-23 19:10:15 -0300

    Program to calculate the area of a triangle

---
+++ CalculateAreaOfTriangle/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    // Declaring the Triangle variable
+    Triangle x, y;
+    x = new Triangle();
+    y = new Triangle();
+
+    // Declaring the Triangle variable X\
+    System.out.println("Enter the measures of triangle X: ");
+    x.a = sc.nextDouble();
+    x.b = sc.nextDouble();
+    x.c = sc.nextDouble();
+
+    // Declaring the Triangle variable Y
+    System.out.println("Enter the measure of triangle Y: ");
+    y.a = sc.nextDouble();
+    y.b = sc.nextDouble();
+    y.c = sc.nextDouble();
+
+    // Calculation of area X
+    double p = (x.a + x.b + x.c) / 2.0;
+    double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+    // Calculation of area Y
+    p = (y.a + y.b + y.c) / 2.0;
+    double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+    // Printing AreaX and AreaY values
+    System.out.printf("Triangle X area: %.4f%n", areaX);
+    System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+    if(areaX > areaY) {
+      System.out.println("Large area X: ");
+    }
+    else {
+      System.out.println("Large area Y: ");
+    }
+
+    sc.close();
+  }
+}
