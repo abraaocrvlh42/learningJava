2419965411c006d11809420152c646368fc92ec3
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-08-13 23:47:12 -0300

    Simple data output

---
+++ SimpleDataOutput/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Abraão";
+        int age = 31;
+        String gender = "male";
+        double salary = 3250.00;
+
+        System.out.printf("%s has %d years old, is %s and receives R$ %.2f reais%n",
+                name, age, gender, salary);
+    }
+}
